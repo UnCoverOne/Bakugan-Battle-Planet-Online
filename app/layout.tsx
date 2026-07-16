dc447042d780709c94d4025a89eb039c67b07052
UnCoverOne <uncover250@gmail.com>
2026-07-16 15:59:41 +0300

    Load official Bakugan typography

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import "./globals.css";
+import "./design-system.css";
 import { NewGameScreenTester } from "../components/game-screen-v2/NewGameScreenTester";
 
 export const metadata: Metadata = {
@@ -21,6 +22,14 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
+        <link
+          rel="stylesheet"
+          href="https://fonts.googleapis.com/css2?family=Lato:ital,wght@0,400;0,700;1,400;1,700&family=Titillium+Web:ital,wght@0,400;0,700;1,400;1,700&display=swap"
+        />
+      </head>
       <body>{children}<NewGameScreenTester /></body>
     </html>
   );
