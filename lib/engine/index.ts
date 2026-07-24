a2082de08e82b34bca6c390ea1f4c2e6eb8555e2
UnCoverOne <uncover250@gmail.com>
2026-07-24 18:06:05 +0300

    engine: expose modern kernel boundary

---
+++ lib/engine/index.ts
@@ -0,0 +1,34 @@
+export { apiActionToCommand, canonicalJson, type ApiAction } from "./commands";
+export {
+  ensureEngineEventStore,
+  loadPersistedCommand,
+  persistInitialMatch,
+  persistTransition,
+  type PersistedCommand,
+} from "./event-store";
+export {
+  ensureEngineMetadata,
+  findCommandReceipt,
+  normalizeEngineState,
+} from "./events";
+export { structuredPhaseFor } from "./phase-machine";
+export { executePlayPipeline, isPlayPipelineCommand, playContextFor, type PlayContext } from "./play-pipeline";
+export { projectEventsForPlayer, projectMatchForPlayer, type PublicGameEvent } from "./projection";
+export { initializeMatch, reduceMatch } from "./reducer";
+export { SeededRandomSource, withDeterministicRuntime, type RandomSource } from "./runtime";
+export {
+  ENGINE_METADATA_KEY,
+  ENGINE_SCHEMA_VERSION,
+  ENGINE_VERSION,
+  RULES_VERSION,
+  EngineCommandError,
+  EngineInvariantError,
+  type CommandEnvelope,
+  type CommandReceipt,
+  type EngineBackedMatchState,
+  type EngineMetadata,
+  type GameCommand,
+  type GameEvent,
+  type ReduceResult,
+  type StructuredPhase,
+} from "./types";
