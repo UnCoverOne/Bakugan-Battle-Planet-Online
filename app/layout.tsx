095b3691965e2f8ffd29ca87ed049f7b90ea4e0b
UnCoverOne <uncover250@gmail.com>
2026-08-01 01:17:54 +0300

    Lazy-load deck inspection from root shell

--- app/layout.tsx
+++ app/layout.tsx
@@ -13,7 +13,7 @@ import { AppShell } from "../components/application/AppShell";
 import { DisplayFontLoader } from "../components/application/DisplayFontLoader";
 import { AssetFreshness } from "../components/AssetFreshness";
 import { WebVitalsReporter } from "../components/WebVitalsReporter";
-import { DeckInspectionLayer } from "../components/game-screen-v2/DeckInspectionLayer";
+import { DeckInspectionHost } from "../components/game-screen-v2/DeckInspectionHost";
 
 export const metadata: Metadata = {
   title: {
@@ -36,7 +36,7 @@ export default function RootLayout({ children }: Readonly<{ children: React.Reac
         <WebVitalsReporter />
         <AppProvider>
           <AppShell>{children}</AppShell>
-          <DeckInspectionLayer />
+          <DeckInspectionHost />
         </AppProvider>
       </body>
     </html>
