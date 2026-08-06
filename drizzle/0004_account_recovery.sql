761c90b556ab88ddd6e0dd72b20bcfc05b871b7e
UnCoverOne <uncover250@gmail.com>
2026-08-06 21:57:52 +0300

    Diagnostic: remove recovery migration

--- drizzle/0004_account_recovery.sql
+++
@@ -1,5 +0,0 @@
-ALTER TABLE `users` ADD `recovery_code_hash` text;
---> statement-breakpoint
-ALTER TABLE `users` ADD `recovery_code_salt` text;
---> statement-breakpoint
-ALTER TABLE `users` ADD `recovery_code_iterations` integer;
