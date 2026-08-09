bbe1059402f94ecec14f06e155b83ee8611a5368
UnCoverOne <uncover250@gmail.com>
2026-08-09 23:01:25 +0300

    Use streamlined lobby room screen

--- app/(workspace)/play/lobby/page.tsx
+++ app/(workspace)/play/lobby/page.tsx
@@ -1,5 +1,5 @@
 import type { Metadata } from "next";
-import { LobbyRoomScreen } from "../../../../components/routes/LobbyRoomScreen";
+import { LobbyRoomScreen } from "../../../../components/routes/StreamlinedLobbyRoomScreen";
 
 export const metadata: Metadata = { title: "Match Lobby" };
 export default function LobbyPage() { return <LobbyRoomScreen />; }
